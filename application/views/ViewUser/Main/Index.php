a8dd40f8a291d3adefc84e671b1bacdc16510159
andikadarma <darmaandika58@gmail.com>
2021-05-10 22:19:24 +0700

    Update Home for User

--- application/views/ViewUser/Main/Index.php
+++ application/views/ViewUser/Main/Index.php
@@ -38,8 +38,8 @@
 						<div class="item">
 							<div class="col-sm-6">
 								<h1><span>J</span>-STORE</h1>
-								<h2>Free Ecommerce Template</h2>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+								<h2>Best Online Shop</h2>
+								<p>Tempat terbaik untuk produk kecantikan. Temukan produk yang cocok untuk anda disini.</p>
 								<button type="button" class="btn btn-default get">Shop Now <i class="fa fa-angle-right"></i></button>
 							</div>
 							<div class="col-sm-6">
