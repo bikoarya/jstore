0d75b9affabdfb99722b581822216761d5c45dda
andikadarma <darmaandika58@gmail.com>
2021-05-10 20:59:43 +0700

    Update About.php

--- application/views/ViewUser/Main/About.php
+++ application/views/ViewUser/Main/About.php
@@ -1,21 +1,21 @@
 <section>
 <div class="container">
     <div class="row">
-        
-        <div class="col-sm-6">
         <h3>About Us</h3>
         <hr>
-        <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat at iusto 
-        et animi amet cumque quod sint aperiam, quis quo quisquam dolor sit, autem 
-        quaerat debitis perferendis in delectus unde?Lorem ipsum dolor sit amet consectetur 
-        adipisicing elit. Suscipit itaque exercitationem aliquid consequuntur neque fugiat tempora corporis quo magnam soluta illum incidunt, tenetur cupiditate ipsa explicabo voluptatibus optio ab non?</div>
+        <div class="col-sm-6">
+            <h5>YOUR ONLINE BEAUTY DESTINATION</h5>
+            <p>J-Store is Indonesia's trusted and most complete online shopping destination offering authentic beauty products e.g. make up, skin care, hair care, and beauty tools serving women across Indonesia.</p>
+            <br>
+            <h5>DELIVERY GUARANTEED</h5>
+            <p>Excellent service is shopping necessity. With our own storage facility, J-Store ensures that product delivery is performed smoothly and product return can be completed hassle-free.</p>
+        </div>
         
-        </p><div class="col-sm-1"></div>
-        <div class="col-sm-5">
-        <h3>Natural Product</h3>
-        <hr>
-        <img src="<?=base_url('assets/images/results/Carosel2.jpg')?>" class="girl img-responsive" alt="" />
+        <div class="col-sm-6">
+            <h5>ONLY THE AUTHENTIC AND BPOM CERTIFIED</h5>
+            <p>Quality and authenticity of beauty product is akin to the importance of taking care of your own skin, a necessity to all women and J-Store takes it very seriously. Usage of non-original or fraudulent beauty products could lead to permanent skin damage.</p>
+            <p>This has prompted J-Store to collaborate directly with authorized national distributors and brand owners in Indonesia to ensure that every product you purchase from J-Store are original and authentic, with the certification from Badan Pengawas Obat dan Makanan (‘BPOM’).</p>
+            <p>Quality assurance of product storage is performed directly by J-Store's own operations management with compliance to international standards and storage guideline by the brand owners.</p>
         </div>
 </div>
 </section>
